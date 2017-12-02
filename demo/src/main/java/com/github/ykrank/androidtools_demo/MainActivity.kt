30ef3d5dd5dd351807409f205ac460ebf220223e
ykrank <ykrank@gmail.com>
2017-12-02 23:59:05 +0800

    Add VideoPlayer library

--- demo/src/main/java/com/github/ykrank/androidtools_demo/MainActivity.kt
+++ demo/src/main/java/com/github/ykrank/androidtools_demo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.ykrank.androidtools_demo
 
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -12,5 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.tvVideo.setOnClickListener { startActivity(Intent(this, FileExplorerActivity::class.java)) }
     }
 }
