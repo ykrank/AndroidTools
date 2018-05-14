d7d1918a75b99e8d155b14a0745a2f5c93964d64
ykrank <ykrank@gmail.com>
2018-05-15 01:50:46 +0800

    Add getNavigationBarHeight method

--- library/src/main/java/com/github/ykrank/androidtools/util/ResourceUtil.java
+++ library/src/main/java/com/github/ykrank/androidtools/util/ResourceUtil.java
@@ -94,4 +94,13 @@ public final class ResourceUtil {
         return ContextCompat.getColorStateList(mContext,
                 ResourceUtil.getResourceId(mContext.getTheme(), android.R.attr.textColorPrimary));
     }
+
+    public static int getNavigationBarHeight(Context context){
+        Resources resources = context.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            return resources.getDimensionPixelSize(resourceId);
+        }
+        return 0;
+    }
 }
