fb3ccfb9a6d8de3e057cd533316d93d1a15977d5
ykrank <ykrank@gmail.com>
2018-07-29 22:27:23 +0800

    Toolbar null exception

--- library/src/main/java/com/github/ykrank/androidtools/extension/FragmentExtension.kt
+++ library/src/main/java/com/github/ykrank/androidtools/extension/FragmentExtension.kt
@@ -9,10 +9,12 @@ import android.support.v7.widget.Toolbar
  * 不依赖Activity的独立初始化Fragment布局中的Toolbar里的menu
  */
 @SuppressLint("RestrictedApi")
-fun Fragment.independentMenu(toolbar: Toolbar) {
-    onCreateOptionsMenu(toolbar.menu, SupportMenuInflater(context))
-    toolbar.setOnMenuItemClickListener {
-        onOptionsItemSelected(it)
-        return@setOnMenuItemClickListener true
+fun Fragment.independentMenu(toolbar: Toolbar?) {
+    if (toolbar != null) {
+        onCreateOptionsMenu(toolbar.menu, SupportMenuInflater(context))
+        toolbar.setOnMenuItemClickListener {
+            onOptionsItemSelected(it)
+            return@setOnMenuItemClickListener true
+        }
     }
 }
