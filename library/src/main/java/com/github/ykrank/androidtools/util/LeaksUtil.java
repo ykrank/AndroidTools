9be92306fac8db1377a02289e1aadea9d2626869
ykrank <ykrank@gmail.com>
2018-12-10 15:06:12 +0800

    Clear dialog memory leak

--- library/src/main/java/com/github/ykrank/androidtools/util/LeaksUtil.java
+++ library/src/main/java/com/github/ykrank/androidtools/util/LeaksUtil.java
@@ -1,6 +1,10 @@
 package com.github.ykrank.androidtools.util;
 
 import android.app.Application;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 
 import com.github.ykrank.androidtools.GlobalData;
 import com.squareup.leakcanary.AndroidExcludedRefs;
@@ -24,6 +28,24 @@ public class LeaksUtil {
                     .buildAndInstall();
         } else {
             return LeakCanary.install(application);
+        }
+    }
+
+    /**
+     * Dialog memory leaks because of Message bug
+     * Must call in onDestroy() in DialogFragment because DismissListener call in onDestroyView()
+     *
+     * @param dialog
+     */
+    public static void clearDialogLeaks(@NonNull Dialog dialog) {
+        dialog.setOnCancelListener(null);
+        dialog.setOnDismissListener(null);
+        dialog.setOnShowListener(null);
+        if (dialog instanceof AlertDialog) {
+            AlertDialog alertDialog = (AlertDialog) dialog;
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, null, null, null);
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, null, null, null);
+            alertDialog.setButton(DialogInterface.BUTTON_NEUTRAL, null, null, null);
         }
     }
 }
