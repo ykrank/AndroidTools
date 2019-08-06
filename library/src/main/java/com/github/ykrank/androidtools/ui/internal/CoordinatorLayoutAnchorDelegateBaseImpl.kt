3afcd85f95cef30165a2a9ebb97386bfdc6e4974
ykrank <ykrank@gmail.com>
2019-08-06 17:25:08 +0800

    Change snackbar max line

--- library/src/main/java/com/github/ykrank/androidtools/ui/internal/CoordinatorLayoutAnchorDelegateBaseImpl.kt
+++ library/src/main/java/com/github/ykrank/androidtools/ui/internal/CoordinatorLayoutAnchorDelegateBaseImpl.kt
@@ -7,6 +7,11 @@ import android.view.View
 import android.widget.Toast
 import com.google.common.base.Optional
 import com.github.ykrank.androidtools.ui.UiGlobalData
+import android.R
+import android.R.id
+import android.widget.TextView
+
+
 
 abstract class CoordinatorLayoutAnchorDelegateBaseImpl(private val mCoordinatorLayout: androidx.coordinatorlayout.widget.CoordinatorLayout) : CoordinatorLayoutAnchorDelegate {
 
@@ -28,12 +33,16 @@ abstract class CoordinatorLayoutAnchorDelegateBaseImpl(private val mCoordinatorL
 
     override fun showShortSnackbar(text: CharSequence): Optional<Snackbar> {
         val snackbar = Snackbar.make(mCoordinatorLayout, text, Snackbar.LENGTH_SHORT)
+        val textView = snackbar.view.findViewById<View>(com.google.android.material.R.id.snackbar_text) as TextView
+        textView.maxLines = SNACK_BAR_MAX_LINE
         snackbar.show()
         return Optional.of(snackbar)
     }
 
     override fun showLongSnackbar(@StringRes resId: Int): Optional<Snackbar> {
         val snackbar = Snackbar.make(mCoordinatorLayout, resId, Snackbar.LENGTH_LONG)
+        val textView = snackbar.view.findViewById<View>(com.google.android.material.R.id.snackbar_text) as TextView
+        textView.maxLines = SNACK_BAR_MAX_LINE
         snackbar.show()
         return Optional.of(snackbar)
     }
@@ -41,6 +50,8 @@ abstract class CoordinatorLayoutAnchorDelegateBaseImpl(private val mCoordinatorL
     override fun showLongSnackbarIfVisible(text: CharSequence, @StringRes actionResId: Int, onClickListener: View.OnClickListener): Optional<Snackbar> {
         if (actLifeCallback?.isAppVisible == true) {
             val snackbar = Snackbar.make(mCoordinatorLayout, text, Snackbar.LENGTH_LONG)
+            val textView = snackbar.view.findViewById<View>(com.google.android.material.R.id.snackbar_text) as TextView
+            textView.maxLines = SNACK_BAR_MAX_LINE
             snackbar.setAction(actionResId, onClickListener)
             snackbar.show()
             return Optional.of(snackbar)
@@ -50,5 +61,9 @@ abstract class CoordinatorLayoutAnchorDelegateBaseImpl(private val mCoordinatorL
 
     override fun dismissSnackbarIfExist() {
         throw UnsupportedOperationException()
+    }
+
+    companion object{
+        const val SNACK_BAR_MAX_LINE = 5
     }
 }
