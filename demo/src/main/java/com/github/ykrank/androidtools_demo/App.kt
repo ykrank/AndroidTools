1eb94ed6c74c72dd97f1188869497e856bad893e
ykrank <ykrank@gmail.com>
2019-10-29 21:25:36 +0800

    Default app provider

--- demo/src/main/java/com/github/ykrank/androidtools_demo/App.kt
+++ demo/src/main/java/com/github/ykrank/androidtools_demo/App.kt
@@ -1,12 +1,9 @@
 package com.github.ykrank.androidtools_demo
 
-import android.app.Application
 import androidx.multidex.MultiDexApplication
 import com.facebook.stetho.Stetho
-import com.github.ykrank.androidtools.AppDataProvider
 import com.github.ykrank.androidtools.DefaultAppDataProvider
 import com.github.ykrank.androidtools.GlobalData
-import com.github.ykrank.androidtools.ui.UiGlobalData
 import com.github.ykrank.androidtools.util.L
 
 class App : MultiDexApplication() {
@@ -15,6 +12,14 @@ class App : MultiDexApplication() {
         super.onCreate()
 
         GlobalData.init(object : DefaultAppDataProvider() {
+            override val logTag: String
+                get() = "AndroidToolsDemo"
+            override val debug: Boolean
+                get() = BuildConfig.DEBUG
+            override val buildType: String
+                get() = BuildConfig.BUILD_TYPE
+            override val appR: Class<out Any>
+                get() = R::class.java
 
         })
         L.init(this)
